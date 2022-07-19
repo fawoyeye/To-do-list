87992bad9d9bf52022e8802d90e350229645eedb
fawoyeye <106695238+fawoyeye@users.noreply.github.com>
2022-07-19 22:12:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+
+
+var name = prompt("Enter your name");
+
+var dayParagraph = document.querySelector(".day");
+var time = document.querySelector(".time");
+var date = new Date();
+var day = date.getDay();
+var minute =date.getMinutes();
+if(minute<10){minute = `0${minute}`};
+var currentHour = date.getHours();
+var hourForTime = date.getHours();
+if(hourForTime>12){hourForTime-=12};
+var add =`${hourForTime}:${minute}`
+switch(day){
+				case 1: dayParagraph.innerHTML="Motivational Monday";
+				break;
+					case 2: dayParagraph.innerHTML="To-do list Tueday";
+				break;
+					case 3: dayParagraph.innerHTML="Working my ass out Wednesday";
+				break;
+					case 4: dayParagraph.innerHTML="Turning it up Thurday";
+				break;
+					case 5: dayParagraph.innerHTML="Fry the work Friday";
+				break;
+					case 6: dayParagraph.innerHTML="Sit up Saturday";
+				break;
+					case 7: dayParagraph.innerHTML="Till sun down Sunday";
+				break;
+				default: "Welcome";}
+			
+			
+			switch(currentHour){
+							case 21: case 22: case 24: case 23: case 1: case 2: case 3: case 4: case 5: time.innerHTML =`It's ${add}, You are awake working? Welldone ${name}`;
+		break;
+		case 6: case 7: case 8: case 9: case 10: case 11: case 12: time.innerHTML =`It's ${add}, Good morning Hard worker!`;
+		break;
+		case 13: case 14: case 15: case 16: time.innerHTML =`It's ${add}, Good afternoon ${name}`;
+		break;
+case 17: case 18: case 19: case 20: time.innerHTML =`It's ${add}, Good Evening ${name}`;
+		break;
+	default: "What time is it?";	
+		
+		
+		
+			}
+			
+			
+			
+			
+			
+			
+			
---
+++ index2.js
@@ -0,0 +1,26 @@
+var quotes = ["If you don't know where you are going, you will probably end up somewhere else. –Lawrence J. Peter","Shoot for the moon. Even if you miss, you'll land among the stars. –Les Brown","Never quit. It is the easiest cop-out in the world. Set a goal and don't quit until you attain it. When you do attain it, set another goal, and don't quit until you reach it. Never quit. –Bear Bryant","A goal is not always meant to be reached; it often serves simply as something to aim at. –Bruce Lee","The great danger for most of us lies not in setting our aim too high and falling short; but in setting our aim too low, and achieving our mark. –Michelangelo","If you aim at nothing, you will hit it every time. –Zig Ziglar","The only limit to the height of your achievements is the reach of your dreams and your willingness to work for them.– Michelle Obama","Setting goals is the first step in turning the invisible into the visible. – Tony Robbins","You measure the size of the accomplishment by the obstacles you have to overcome to reach your goals. - Booker T. Washington","Never give up. Today is hard, tomorrow will be worse, but the day after tomorrow will be sunshine. - Jack Ma"];
+
+window.onload = function newQuote(){
+var num = Math.floor(Math.random() * (quotes.length));
+		var quo = document.querySelector('.quo');
+quo.innerHTML = quotes[num];		
+}
+var space = document.querySelector('.space');
+var button = document.querySelector('button');
+var inputField = document.querySelector('input');
+var container = document.querySelector('.toDoContainer');
+button.onclick = function(){
+				
+				var create = document.createElement('p');
+				
+				create.innerHTML = inputField.value;
+			container.appendChild(create);
+			inputField.value="";
+			create.classList.add('paragraph');
+			create.onclick = function(){create.style.textDecoration="line-through"};
+			create.ondblclick =function(){create.style.display="none"};
+			
+			}
+		function al(){	alert("Click on your text to cross it out and double click on it to delete it");}
+
+setTimeout(al, 5000);
